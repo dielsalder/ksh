08d6969a9395c3e8ad0145345480330af9c74e56
He <yhe0405@sh-rm214-lt15.greatneck.k12.ny.us>
2015-01-07 10:05:01 -0500

    output functions for split

--- split.py
+++ split.py
@@ -6,26 +6,38 @@ import ksh
 
 class Split(pdb.Dna):
     def __init__(self):
-        self.all_res = {}
+        self.res_all = {}
 
     def split(self, ibp):
         """Split helix at base pair ibp"""
-        self.split = ibp
+        self.i_split = ibp
         self.front = pdb.crdset([a for a in self.atoms if a['pair'] < ibp])
         self.rear = pdb.crdset([a for a in self.atoms if a['pair'] > ibp])
         return self.front, self.rear
 
     def eval_res(self):
-        """Calculate fit for current split and store res in self.all_res"""
+        """Calculate fit for current split and store res in self.res_all"""
         res_f = self.minimize((self.front).res)
         res_r = self.minimize((self.rear).res)
         res_sum = res_f + res_r
-        self.all_res[self.split] = sum_res
+        self.res_all[self.i_split] = sum_res
         return sum_res
 
     def iterbp(self):
-        """Fill all_res with res from each bp and find min"""
+        """Fill res_all with res from each bp"""
         for ibp in (2, self.numbp - 2):
             self.split(ibp)
             self.eval_res()
-        self.res_min = min(self.all_res, key = self.all_res.get)
+        return self.res_all
+
+    def fmin_res(self):
+        """Find ibp of minimum residual in res_all"""
+        self.bp_min = min(self.res_all, key = self.res_all.get)
+        return self.bp_min
+
+    def min_crdset(self):
+        """Evaluate helix parameters for minimum split"""
+        self.split(ibp)
+        self.front.calc_all()
+        self.rear.calc_all()
+        return self.front, self.rear, self.i_split
